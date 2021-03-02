af314c45b5545ee18dad1a4b664fb69d226e684f
abdullah-ch <abdullahchaudharry@gmail.com>
2021-03-03 00:46:07 +0500

    imported the JSON array

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React from "react";
- 
 import SimpleAccordion from "./simpleAccordion";
 import "./Sidebar.css";
-import {brands,stores,catogeries,price} from "./allData.js";
+import { brands, stores, catogeries, price } from "./allData.js";
+import data from "./Data.json";
 
 function App() {
-  console.log("I am in App component", brands);
-// <hr style={{ color: "#999999"}}/>
+  console.log("I am the Json Data", data);
+  // <hr style={{ color: "#999999"}}/>
   return (
     <div className="sidenav">
-    <SimpleAccordion  children="Categories" arrayName={catogeries} />
-   
-   <SimpleAccordion  children="Brands" arrayName={brands}/>
-  
-    <SimpleAccordion  children="Stores" arrayName={stores} /> 
-    
-    <SimpleAccordion  children="Price" arrayName={price} />  
-</div>
+      <SimpleAccordion children="Categories" arrayName={catogeries} />
+
+      <SimpleAccordion children="Brands" arrayName={brands} />
+
+      <SimpleAccordion children="Stores" arrayName={stores} />
+
+      <SimpleAccordion children="Price" arrayName={price} />
+    </div>
   );
-  }
-  export default App;
+}
+export default App;
